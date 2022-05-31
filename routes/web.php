f4edbfe8bef38b071aa97431d6e3b916dd35c2fe
Edwin Rafael Rosario De La Rosa <rosarioedwinac@gmail.com>
2022-05-30 20:27:01 -0500

    Correcion

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Reddatasrd\NumTostring\NumTostring;
 use Illuminate\Support\Facades\Route;
 use Reddatasrd\NumTostring\NumTostringFacade;
 
+/**
+ * pueba
+ */
 Route::get('/cal', function(){
     $nu=5;
     return NumTostring::string($nu);
